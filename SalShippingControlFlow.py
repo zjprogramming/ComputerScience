baa2d098f7e7eb4a43f0fa3048b4d1cff1e4dd2b
zjprogramming <145306561+zjprogramming@users.noreply.github.com>
2023-09-18 19:36:13 -0700

    Add files via upload

---
+++ SalShippingControlFlow.py
@@ -0,0 +1,27 @@
+weight = 41.5
+# "Ground Shipping"
+if weight <= 2:
+  cost_ground = weight *1.5 + 20
+elif weight <= 6:
+  cost_ground = weight * 3 + 20
+elif weight <=10:
+  cost_ground = weight * 4 + 20
+else:
+  cost_ground = weight * 4.75 + 20
+
+print("Ground Shipping $", cost_ground)
+
+cost_premium = 125
+print("Ground Shipping Premium $", cost_premium)
+
+#drone shipping
+
+if weight <= 2:
+  cost_drone = weight *4.5
+elif weight <= 6:
+  cost_drone = weight * 9
+elif weight <=10:
+  cost_drone = weight * 12
+else:
+  cost_drone = weight * 14.25
+print("Drone Shipping $", cost_drone)
